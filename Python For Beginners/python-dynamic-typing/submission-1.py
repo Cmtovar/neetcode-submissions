a7fed21fda6be6a87b7c61092d8f86ed511d6fd8
Cristian Tovar <ctov02@gmail.com>
2026-08-01 08:47:03 -0500

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 0
+print(type(variable))
+variable = 99.0
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "World"
+print(type(variable))
+variable = [6, 7, 8]
+print(type(variable))
